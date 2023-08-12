1c3234559d29a7e6b8e19908fa3033aba89b2574
watcharapol Yotadee <watcharapolyotadee@watcharapols-MacBook-Pro.local>
2023-08-12 23:45:39 +0700

    feat: add exercise-001 soft stuio

---
+++ Computer-Programming/week4/lottery-6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+int main(){
+    int inputNumber;
+    printf("*** Lottery ***\n");
+    printf("Ticket Number :");
+    scanf("%d",&inputNumber);
+    
+
+    
+    
+    
+    return 0;
+}
