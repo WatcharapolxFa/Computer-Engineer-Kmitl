38faf8c23ccd05bd41e90f1b95e292bfd2991c12
WatcharapolxFa <63010870@kmitl.ac.th>
2023-08-29 15:10:19 +0700

    feat: add week 2 computer-pro

---
+++ Computer-Programming/week2/hw1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main (){
+    printf("My name is : Watcharapol \n");
+    printf("My nickname is : Far \n");
+    printf("My telephone number is : 098-xxx-xxxx \n");
+    return 0;
+}
---
+++ Project-kmitl/Description.md
@@ -0,0 +1,2 @@
+# Study materials and homework problem files
+https://www.notion.so/Software-Studio-e933b7e51cde4e21956faed6e103c541?pvs=4
